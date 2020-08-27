03f62f8592c38e70b3f9276619baf1f59c711956
Daan de jager <daandejager2002@gmail.com>
2020-08-27 20:39:35 +0200

    5-6

--- 1-Arrays/taak04/index.php
+++ 1-Arrays/taak04/index.php
@@ -18,22 +18,35 @@ $speelgoed = [
         'prijs' => '39.00'
     ],
 ];
+// $speelgoed = array (
+//     array("Bal","sport","5.00"),
+//     array("knuffelbeer","knuffels","9,50"),
+//     array("Catan","bordspel","39.00"),
+//   );
 
 
 $games = [
-    'sport' => [
+    'sport'=>[
         'naam' => 'Fifa',
         'uitgever' => 'EA',
         'prijs' => '60.00'
     ],
-    'action' => [
+    'Actie'=>[
         'naam' => 'Doom',
         'uitgever' => 'Bethesda',
         'prijs' => '60.00'
     ],
-    'fps' => [
+    'fps'=>[
         'naam' => 'Left for dead 2',
         'uitgever' => 'Valve',
         'prijs' => '40.00'
     ]
 ];
+echo "Speelgoed " . $speelgoed[0]["naam"]." uit de categorie ".$speelgoed[0]["soort"]." kost ".$speelgoed[0]["prijs"].".<br>";
+echo "Speelgoed " . $speelgoed[1]["naam"]." uit de categorie ".$speelgoed[1]["soort"]." kost ".$speelgoed[1]["prijs"].".<br>";
+echo "Speelgoed " . $speelgoed[2]["naam"]." uit de categorie ".$speelgoed[2]["soort"]." kost ".$speelgoed[2]["prijs"].".<br>";
+echo "Speelgoed " . $games['sport']['naam']." van de uitgever ".$games['sport']["uitgever"]." kost ".$games['sport']["prijs"].".<br>";
+echo "Speelgoed " . $games["Actie"]["naam"]." van de uitgever ".$games["Actie"]["uitgever"]." kost ".$games["Actie"]["prijs"].".<br>";
+echo "Speelgoed " . $games['fps']["naam"]." van de uitgever ".$games['fps']["uitgever"]." kost ".$games["fps"]["prijs"].".<br>";
+
+?>
