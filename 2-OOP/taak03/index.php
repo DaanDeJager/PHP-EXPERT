e7927d58b31c76e026516b0c63258ce88cc8a612
Daan de jager <daandejager2002@gmail.com>
2020-08-28 13:50:28 +0200

    f

---
+++ 2-OOP/taak03/index.php
@@ -0,0 +1,19 @@
+<?php
+class product{
+    public $merk;
+    public $prijs;
+    public $type;
+    public $kleur;
+}
+$dweil = new product();
+$stofzuiger = new product();
+$stofzuiger->merk = ("aegon");
+$stofzuiger->prijs = ("50");
+$stofzuiger->type = ("ggfs505");
+$stofzuiger->kleur = ("grijs");
+$dweil->merk = ("hema");
+$dweil->prijs = (10);
+$dweil->type = ("lang");
+$dweil->kleur = ("rood");
+
+?>
