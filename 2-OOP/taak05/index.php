686b7953b7c90037029c4fbaab8050cb2c7b23c4
Daan de jager <daandejager2002@gmail.com>
2020-08-30 16:47:09 +0200

    6

--- 2-OOP/taak05/index.php
+++ 2-OOP/taak05/index.php
@@ -9,6 +9,12 @@ class lamp{
 }
 $woonkamerLamp = new lamp();
 $keukenLamp = new lamp();
+$woonkamerLamp->kleur = ("geel");
+$keukenLamp->kleur = ("wit");
+$woonkamerLamp->helderheid = ("zacht");
+$keukenLamp->helderheid= ("vel");
+$woonkamerLamp->isAan = ("true");
+$keukenLamp->isAan = ("false");
 $woonkamerLamp->setStatus("true");
 $keukenLamp->setStatus("false");
 echo $woonkamerLamp->status;
